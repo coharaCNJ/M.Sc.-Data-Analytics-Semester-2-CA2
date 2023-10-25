cb893636d6d51e61515a95714055ab505d7ebe05
c-o-hara <46498951+c-o-hara@users.noreply.github.com>
2023-10-25 17:39:37 +0100

    Mapper_Reducer_finalised_Further_Sentiment_Analysis

--- tweet_mapper.py
+++ tweet_mapper.py
@@ -13,6 +13,45 @@ import sys
 for line in sys.stdin:
         data = line.strip().split(",")
         if len(data) == 6:
-                # These are names of the columns/ attributes given to the data file
-                Num,tweetID, Date, Query, Username, Tweet = data
-                print("{0},{1}".format(Date, Tweet))
+                Num, tweetID, Date, Query, Username, Tweet = data
+                print("{0},{1},{2}".format(Num, Date, Tweet))
+        if len(data) == 7:
+                Num = data[0]
+                Date = data[2]
+                Tweet = data[5] + data[6]
+                print("{0},{1},{2}".format(Num, Date, Tweet))
+        if len(data) == 8:
+                Num = data[0]
+                Date = data[2]
+                Tweet = data[5] + data[6] + data[7]
+                print("{0},{1},{2}".format(Num, Date, Tweet))
+        if len(data) == 9:
+                Num = data[0]
+                Date = data[2]
+                Tweet = data[5] + data[6] + data[7] + data[8]
+                print("{0},{1},{2}".format(Num, Date, Tweet))
+        if len(data) == 10:
+                Num = data[0]
+                Date = data[2]
+                Tweet = data[5] + data[6] + data[7] + data[8] + data[9]
+                print("{0},{1},{2}".format(Num, Date, Tweet))
+        if len(data) == 11:
+                Num = data[0]
+                Date = data[2]
+                Tweet = data[5] + data[6] + data[7] + data[8] + data[9] + data[10]
+                print("{0},{1},{2}".format(Num, Date, Tweet))
+        if len(data) == 12:
+                Num = data[0]
+                Date = data[2]
+                Tweet = data[5] + data[6] + data[7] + data[8] + data[9] + data[10]+ data[11]
+                print("{0},{1},{2}".format(Num, Date, Tweet))
+        if len(data) == 13:
+                Num = data[0]
+                Date = data[2]
+                Tweet = data[5] + data[6] + data[7] + data[8] + data[9] + data[10] + data[11] + data[12]
+                print("{0},{1},{2}".format(Num, Date, Tweet))
+        if len(data) >= 14:
+                Num = data[0]
+                Date = data[2]
+                Tweet = data[5] + data[6] + data[7] + data[8] + data[9] + data[10] + data[11] + data[12] + data[13]
+                print("{0},{1},{2}".format(Num, Date, Tweet))
--- tweet_reducer.py
+++ tweet_reducer.py
@@ -5,25 +5,28 @@ import re
 
 tweet = None
 oldKey = None
+Date = None
 
 # It will be in the format key,val
 # Where key is the date, val is the tweet with all non-alpha characters removed
 
 for line in sys.stdin:
     data_mapped = line.strip().split(",")
-    if len(data_mapped) != 2:
+    if len(data_mapped) != 3:
         # Something has gone wrong. Skip this line.
         continue
 
-    thisKey, thisTweet = data_mapped
+    thisKey, date, thisTweet = data_mapped
 
     if oldKey and oldKey != thisKey:
-        print(oldKey, ",", tweet)
+        print(oldKey, ",", Date, ",", tweet)
         oldKey = thisKey;
-        tweet = re.sub(r'[^\w\s]', '', thisTweet)
+        tweet = re.sub(r'[^\w\s]', '', thisTweet);
+        Date = date;
 
     oldKey = thisKey
     tweet = re.sub(r'[^\w\s]', '', thisTweet)
+    Date = date
 
 if oldKey != None:
-    print(oldKey, ",", tweet)
+    print(oldKey, ",", Date, ",", tweet)
