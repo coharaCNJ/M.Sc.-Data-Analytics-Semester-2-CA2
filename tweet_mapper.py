3113c3f2d465378b771b7e7583225904ce326e3a
c-o-hara <46498951+c-o-hara@users.noreply.github.com>
2023-11-06 18:25:28 +0000

    Cleaning up and writing report

--- tweet_mapper.py
+++ tweet_mapper.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-# extract Date and Tweet from each entry
+# extract tweetID, Date and Tweet from each entry
 # An axploration on the data shows that there are no queries associated with any tweet, and username is not something
-# that will be analysed here, so everything except date and tweet can be dropped from the raw data.
+# that will be analysed here, so everything except tweet Num (unique) date, and tweet, can be dropped from the raw data.
 
 # Format of each line is:
 # tweetID\Date\Query\Username\Tweet
@@ -13,45 +13,12 @@ import sys
 for line in sys.stdin:
         data = line.strip().split(",")
         if len(data) == 6:
-                Num, tweetID, Date, Query, Username, Tweet = data
-                print("{0},{1},{2}".format(Num, Date, Tweet))
-        if len(data) == 7:
-                Num = data[0]
-                Date = data[2]
-                Tweet = data[5] + data[6]
-                print("{0},{1},{2}".format(Num, Date, Tweet))
-        if len(data) == 8:
-                Num = data[0]
-                Date = data[2]
-                Tweet = data[5] + data[6] + data[7]
-                print("{0},{1},{2}".format(Num, Date, Tweet))
-        if len(data) == 9:
-                Num = data[0]
-                Date = data[2]
-                Tweet = data[5] + data[6] + data[7] + data[8]
-                print("{0},{1},{2}".format(Num, Date, Tweet))
-        if len(data) == 10:
-                Num = data[0]
-                Date = data[2]
-                Tweet = data[5] + data[6] + data[7] + data[8] + data[9]
-                print("{0},{1},{2}".format(Num, Date, Tweet))
-        if len(data) == 11:
-                Num = data[0]
-                Date = data[2]
-                Tweet = data[5] + data[6] + data[7] + data[8] + data[9] + data[10]
-                print("{0},{1},{2}".format(Num, Date, Tweet))
-        if len(data) == 12:
-                Num = data[0]
-                Date = data[2]
-                Tweet = data[5] + data[6] + data[7] + data[8] + data[9] + data[10]+ data[11]
-                print("{0},{1},{2}".format(Num, Date, Tweet))
-        if len(data) == 13:
-                Num = data[0]
-                Date = data[2]
-                Tweet = data[5] + data[6] + data[7] + data[8] + data[9] + data[10] + data[11] + data[12]
-                print("{0},{1},{2}".format(Num, Date, Tweet))
-        if len(data) >= 14:
-                Num = data[0]
-                Date = data[2]
-                Tweet = data[5] + data[6] + data[7] + data[8] + data[9] + data[10] + data[11] + data[12] + data[13]
-                print("{0},{1},{2}".format(Num, Date, Tweet))
+            Num, tweetID, Date, Query, Username, Tweet = data
+            print("{0},{1},{2}".format(tweetID, Date, Tweet))
+        elif len(data) > 6:
+            tweetID = data[1]
+            Date = data[2]
+            Tweet = data[5]
+            for y in range(6,len(data)):
+                Tweet = Tweet + " " + data[y]
+            print("{0},{1},{2}".format(tweetID, Date, Tweet))
